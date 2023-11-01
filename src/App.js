a3bdccd14d35ba82a1860e66b1184243308642f7
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-01 12:53:34 +0300

    Added handleClick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    console.log('clicked');
+   };
+
   return (
     <div className="drum-machine">
       <div className="display">
-        <div className="drum-pad" id="">Q<audio src="" className="clip" id="Q"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>Q<audio src="./sounds/Heater-1.mp3" className="clip" id="Q"></audio></div>
         <div className="drum-pad" id="">W<audio src="./sounds/" className="clip" id="W"></audio></div>
         <div className="drum-pad" id="">E<audio src="./sounds/" className="clip" id="E"></audio></div>
         <div className="drum-pad" id="">A<audio src="./sounds/" className="clip" id="A"></audio></div>
