84e0dc5ae038b27661900b06548846d387d5a2d5
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-01 13:16:03 +0300

    Changed audio sources to online links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 
 function App() {
-  const handleClick = () => {
-    console.log('clicked');
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    let audio = document.getElementById(e.target.innerText);
+    audio.play();
    };
+  
+
 
   return (
     <div className="drum-machine">
       <div className="display">
-        <div className="drum-pad" id="" onClick={handleClick}>Q<audio src="./sounds/Heater-1.mp3" className="clip" id="Q"></audio></div>
-        <div className="drum-pad" id="">W<audio src="./sounds/" className="clip" id="W"></audio></div>
-        <div className="drum-pad" id="">E<audio src="./sounds/" className="clip" id="E"></audio></div>
-        <div className="drum-pad" id="">A<audio src="./sounds/" className="clip" id="A"></audio></div>
-        <div className="drum-pad" id="">S<audio src="./sounds/" className="clip" id="S"></audio></div>
-        <div className="drum-pad" id="">D<audio src="./sounds/" className="clip" id="D"></audio></div>
-        <div className="drum-pad" id="">Z<audio src="./sounds/" className="clip" id="Z"></audio></div>
-        <div className="drum-pad" id="">X<audio src="./sounds/" className="clip" id="X"></audio></div>
-        <div className="drum-pad" id="">C<audio src="./sounds/" className="clip" id="C"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>Q<audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' className="clip" id="Q"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>W<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip" id="W"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>E<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" id="E"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>A<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip" id="A"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>S<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className="clip" id="S"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>D<audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" id="D"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>Z<audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip" id="Z"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>X<audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip" id="X"></audio></div>
+        <div className="drum-pad" id="" onClick={handleClick}>C<audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip" id="C"></audio></div>
       </div>
     </div>
   );
