3ec95828c6f834fdfe902a0d695251b42319e368
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-01 12:50:45 +0300

    Added drumpads, audios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="drum-machine">
+      <div className="display">
+        <div className="drum-pad" id="">Q<audio src="" className="clip" id="Q"></audio></div>
+        <div className="drum-pad" id="">W<audio src="./sounds/" className="clip" id="W"></audio></div>
+        <div className="drum-pad" id="">E<audio src="./sounds/" className="clip" id="E"></audio></div>
+        <div className="drum-pad" id="">A<audio src="./sounds/" className="clip" id="A"></audio></div>
+        <div className="drum-pad" id="">S<audio src="./sounds/" className="clip" id="S"></audio></div>
+        <div className="drum-pad" id="">D<audio src="./sounds/" className="clip" id="D"></audio></div>
+        <div className="drum-pad" id="">Z<audio src="./sounds/" className="clip" id="Z"></audio></div>
+        <div className="drum-pad" id="">X<audio src="./sounds/" className="clip" id="X"></audio></div>
+        <div className="drum-pad" id="">C<audio src="./sounds/" className="clip" id="C"></audio></div>
+      </div>
+    </div>
   );
 }
 
