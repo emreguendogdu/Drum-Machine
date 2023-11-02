a196f06fa3edc98c749bc2fdc59ab928daea180d
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-02 11:33:57 +0300

    Changed every drum-pad with new Drumpad component, added playPromise for audios to fix audio.pause error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import './App.css';
+import Drumpad from "./Drumpad";
 
 function App() {
-  const handleClick = (e) => {
-    console.log(e.target.innerText);
-    let audio = document.getElementById(e.target.innerText);
-    audio.play();
-   };
-  
 
+  function handleClick (e) {
+    const audio = document.getElementById(e.target.innerText);
+    console.log(audio);
+    audio.currentTime = 0; // reset the current time
+    const playPromise = audio.play();
+    if (playPromise !== undefined) {
+    playPromise.then(_ => {
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  };
+  
+  function handleKey (e) {
+    const key = e.key.toUpperCase(); 
+    const audio = document.getElementById(key);
+    if (!audio) return; 
+    audio.currentTime = 0; 
+    const playPromise = audio.play();
+    if (playPromise !== undefined) {
+    playPromise.then(_ => {
+    })
+    .catch(error => {
+    });
+  }
+  }
+
+  window.addEventListener('keydown', handleKey);
 
   return (
-    <div className="drum-machine">
-      <div className="display">
-        <div className="drum-pad" id="" onClick={handleClick}>Q<audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' className="clip" id="Q"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>W<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip" id="W"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>E<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" id="E"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>A<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip" id="A"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>S<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className="clip" id="S"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>D<audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" id="D"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>Z<audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip" id="Z"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>X<audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip" id="X"></audio></div>
-        <div className="drum-pad" id="" onClick={handleClick}>C<audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip" id="C"></audio></div>
+    <div id="drum-machine">
+      <div id="display">
+        <Drumpad handleClick={handleClick} k={"Q"} audiosrc={"Heater-1.mp3"} name={"Heater 1"}/>
+        <Drumpad handleClick={handleClick} k={"W"} audiosrc={"Heater-2.mp3"} name={"Heater 2"}/>
+        <Drumpad handleClick={handleClick} k={"E"} audiosrc={"Heater-3.mp3"} name={"Heater 3"}/>
+        <Drumpad handleClick={handleClick} k={"A"} audiosrc={"Heater-4_1.mp3"} name={"Heater 4"}/>
+        <Drumpad handleClick={handleClick} k={"S"} audiosrc={"Heater-6.mp3"} name={"Clap"}/>
+        <Drumpad handleClick={handleClick} k={"D"} audiosrc={"Dsc_Oh.mp3"} name={"Open Hi-hat"}/>
+        <Drumpad handleClick={handleClick} k={"Z"} audiosrc={"Kick_n_Hat.mp3"} name={"Kick n' Hat"}/>
+        <Drumpad handleClick={handleClick} k={"X"} audiosrc={"RP4_KICK_1.mp3"} name={"Kick"}/>
+        <Drumpad handleClick={handleClick} k={"C"} audiosrc={"Cev_H2.mp3"} name={"Closed Hi-hat"}/>
       </div>
     </div>
   );
