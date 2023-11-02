176979002725b308b951e62565947cb682d81c16
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-02 11:32:34 +0300

    Changed props.key to props.k to prevent conflict with default key attribute

--- src/Drumpad.js
+++ src/Drumpad.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Drumpad(props) {
   return (
-    <div id='drumpad' onClick={props.handleClick}>{props.key}<audio src={"https://s3.amazonaws.com/freecodecamp/drums/" + props.audiosrc} className='clip' id={props.key} data-name={props.name}></audio></div>
+    <div className='drum-pad' onClick={props.handleClick}>{props.k}<audio src={"https://s3.amazonaws.com/freecodecamp/drums/" + props.audiosrc} className='clip' id={props.k} data-name={props.name}></audio></div>
   )
 }
