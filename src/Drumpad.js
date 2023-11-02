6443edde86f1128fa775ef5b0d63a179c057c1d0
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-02 11:44:03 +0300

    Changed innerText to kbd, re-formatted the overall code for readability

--- src/Drumpad.js
+++ src/Drumpad.js
@@ -3,6 +3,12 @@ import './Drumpad.scss'
 
 export default function Drumpad(props) {
   return (
-    <div className='drum-pad' onClick={props.handleClick}>{props.k}<audio src={"https://s3.amazonaws.com/freecodecamp/drums/" + props.audiosrc} className='clip' id={props.k} data-name={props.name}></audio></div>
+    <div className='drum-pad' 
+    onClick={props.handleClick}>
+        <kbd>{props.k}</kbd>
+        <audio src={"https://s3.amazonaws.com/freecodecamp/drums/" + props.audiosrc} className='clip' 
+        id={props.k} data-name={props.name}>
+
+        </audio></div>
   )
 }
