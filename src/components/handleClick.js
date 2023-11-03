16ee039c57eeaca2ed6d3992eb89ff3858d335b7
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-03 13:03:50 +0300

    Increased readability, added explanations

--- src/components/handleClick.js
+++ src/components/handleClick.js
@@ -1,11 +1,32 @@
-export function handleClick (e) {
-  const audio = document.getElementById(e.target.innerText);
-  if (!audio) return;
-  audio.currentTime = 0; // reset the current time
-  audio.play();
-  const playingDiv = document.querySelector(".drum-pad[data-key=" + e.target.innerText + "]");
-  playingDiv.classList.add("playing");
-  setTimeout(() => {
-    playingDiv.classList.remove("playing");
-  }, 200); 
+export function handleClick(e) {
+    // Get the key that was clicked
+    const key = e.target.innerText; 
+
+    // Get the audio and display element
+    const audio = document.getElementById(key);
+    const display = document.getElementById("display");
+
+    // If the audio element is not found, break out
+    if (!audio) return;
+
+    // Reset audio and play it
+    audio.currentTime = 0; 
+    audio.play();
+
+    // Get the drum pad element that was clicked
+    const playingDiv = document.querySelector(`.drum-pad[data-key=${key}]`); 
+
+    // Add "playing" class to the drum pad and display elements
+    playingDiv.classList.add("playing"); 
+    display.classList.add("playing"); 
+
+    // Set the display text to the name of the audio clip
+    display.innerText = audio.getAttribute("data-name");
+
+    // Remove "playing" class and text of display after 200ms
+    setTimeout(() => {
+        playingDiv.classList.remove("playing");
+        display.classList.remove("playing"); 
+        display.innerText = "";
+    }, 200); 
 }
