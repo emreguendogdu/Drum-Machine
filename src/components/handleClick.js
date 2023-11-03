10ef9690748fcaca4678ccdac0c741010ed9ee11
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-03 10:58:17 +0300

    Moved handleClick to components

---
+++ src/components/handleClick.js
@@ -0,0 +1,11 @@
+export function handleClick (e) {
+  const audio = document.getElementById(e.target.innerText);
+  if (!audio) return;
+  audio.currentTime = 0; // reset the current time
+  audio.play();
+  const playingDiv = document.querySelector(".drum-pad[data-key=" + e.target.innerText + "]");
+  playingDiv.classList.add("playing");
+  setTimeout(() => {
+    playingDiv.classList.remove("playing");
+  }, 200); 
+}
